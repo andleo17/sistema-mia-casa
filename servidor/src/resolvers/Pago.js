cddf7dfebb9f6757e35a4480dd09407c7c4fddfa
jcarlos5 <josecarlosvilchezvillegas@gmail.com>
2020-06-24 12:04:48 -0500

    pago

--- servidor/src/resolvers/Pago.js
+++ servidor/src/resolvers/Pago.js
@@ -25,18 +25,51 @@ function getMutations() {
 	return {
 		registrarPago: async (parent, args, context) => {
 			const data = {
-				serie: args.serie,
-				numero: args.numero,
-				fecha: args.fecha,
+				serie: "sre",
+				numero: "ff",
 				monto: args.monto,
 				tipoPago: { connect: { id: parseInt(args.tipoPago) } },
 				pedido: { connect: { id: parseInt(args.pedido) } },
 			};
+			await context.prisma.pedido
+				.update({
+					where: {id: parseInt(args.pedido)},
+					data: {estado: false},
+				})
+				.catch((err) => null);
+			const pedido = await context.prisma.pedido.findOne({
+				where: {id: parseInt(args.pedido)}
+			});
+			const mesa = pedido.mesaId;
+			await context.prisma.mesa
+				.update({
+					where: {id: mesa},
+					data: {ocupado: false},
+				})
+				.catch((err) => err);
 			return await context.prisma.pago
 				.create({ data })
-				.catch((err) => null);
+				.catch((err) => err);
 		},
 		eliminarPago: async (parent, args, context) => {
+			const pago = await context.prisma.pago.findOne({
+				where: {id: parseInt(args.id)}
+			});
+			await context.prisma.pedido
+				.update({
+					where: {id: pago.pedidoId},
+					data: {estado: true},
+				})
+				.catch((err) => null);
+			const pedido = await context.prisma.pedido.findOne({
+				where: {id: pago.pedidoId}
+			});
+			await context.prisma.mesa
+				.update({
+					where: {id: pedido.mesaId},
+					data: {ocupado: true},
+				})
+				.catch((err) => err);
 			return await context.prisma.pago
 				.delete({ where: { id: parseInt(args.id) } })
 				.catch((err) => null);
