ca4e034a6239764168830f184e809826046ebbb8
Andres Baldarrago <andres.gastulo@gmail.com>
2020-07-11 21:17:38 -0500

    Terminando mesa.

--- src/graphql/resolvers/Mesa.js
+++ src/graphql/resolvers/Mesa.js
@@ -1,54 +1,58 @@
-async function pedidoActual(parent, args, context) {
-	const pedidos = await context.prisma.mesa
-		.findOne({ where: { id: parent.id } })
+import { AuthenticationError } from 'apollo-server';
+import { CAMPO_NO_ADMIN } from '../../utils/errors';
+
+async function pedidoActual({ id }, args, { prisma }) {
+	const pedidos = await prisma.mesa
+		.findOne({ where: { id } })
 		.pedidosRealizados({ where: { estado: true } });
 	return pedidos[0];
 }
 
-async function pedidosRealizados(parent, args, context) {
-	return await context.prisma.mesa
-		.findOne({ where: { id: parent.id } })
-		.pedidosRealizados();
+async function pedidosRealizados({ id }, args, { usuario, prisma }) {
+	if (usuario.rol !== 'ADMIN')
+		throw new AuthenticationError(CAMPO_NO_ADMIN('pedidos realizados'));
+	return await prisma.mesa.findOne({ where: { id } }).pedidosRealizados({
+		skip: (args.pagina - 1) * args.cantidad || undefined,
+		take: args.cantidad,
+	});
 }
 
-async function listarMesa(parent, args, context) {
-	return await context.prisma.mesa.findMany();
+async function listarMesa(parent, args, { usuario, prisma }) {
+	const where = {};
+	if (usuario.rol !== 'ADMIN') where.estado = true;
+	return await prisma.mesa.findMany({
+		where,
+		skip: (args.pagina - 1) * args.cantidad || undefined,
+		take: args.cantidad,
+		orderBy: { numero: 'asc' },
+	});
 }
 
-async function registrarMesa(parent, args, context) {
-	const data = {
-		numero: args.numero,
-	};
-	return await context.prisma.mesa.create({ data }).catch((err) => null);
-}
-
-async function modificarMesa(parent, args, context) {
-	const data = {};
-	if (args.numero) data.numero = args.numero;
-	if (args.ocupado != null) data.ocupado = args.ocupado;
-	if (args.estado != null) data.estado = args.estado;
-	return await context.prisma.mesa
-		.update({
-			where: { id: parseInt(args.id) },
-			data,
-		})
+async function registrarMesa(parent, args, { prisma }) {
+	return await prisma.mesa
+		.create({ data: { numero: args.numero } })
 		.catch((err) => null);
 }
 
-async function eliminarMesa(parent, args, context) {
-	const numeroPedido = await context.prisma.pedido.count({
-		where: { mesaId: parseInt(args.id) },
+async function modificarMesa(parent, args, { prisma }) {
+	const data = { numero: args.numero, estado: args.estado };
+	return await prisma.mesa
+		.update({ where: { id: parseInt(args.id) }, data })
+		.catch((err) => null);
+}
+
+async function eliminarMesa(parent, { id }, { prisma }) {
+	const numeroPedido = await prisma.pedido.count({
+		where: { mesaId: parseInt(id) },
 	});
-	if (numeroPedido == 0) {
-		return await context.prisma.mesa
-			.delete({ where: { id: parseInt(args.id) } })
+	if (numeroPedido === 0) {
+		return await prisma.mesa
+			.delete({ where: { id: parseInt(id) } })
 			.catch((err) => null);
 	} else {
-		return await context.prisma.mesa.update({
-			where: { id: parseInt(args.id) },
-			data: {
-				estado: false,
-			},
+		return await prisma.mesa.update({
+			where: { id: parseInt(id) },
+			data: { estado: false },
 		});
 	}
 }
