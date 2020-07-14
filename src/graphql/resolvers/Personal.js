6650b225907a1eaf0c74ef1eed172f0c98abfd76
Andres Baldarrago <andres.gastulo@gmail.com>
2020-07-14 11:20:59 -0500

    Implementando Personal.

--- src/graphql/resolvers/Personal.js
+++ src/graphql/resolvers/Personal.js
@@ -1,69 +1,70 @@
-async function cargo(parent, args, context) {
-	return await context.prisma.personal
-		.findOne({ where: { id: parent.id } })
-		.cargo();
+import { AuthenticationError } from 'apollo-server';
+import { NO_ADMIN } from '../../utils/errors';
+
+async function cargo({ id }, args, { prisma }) {
+	return await prisma.personal.findOne({ where: { id } }).cargo();
 }
 
-async function credencial(parent, args, context) {
-	return await context.prisma.personal
-		.findOne({ where: { id: parent.id } })
-		.credencial();
+async function credencial({ id }, args, { prisma }) {
+	return await prisma.personal.findOne({ where: { id } }).credencial();
 }
 
-async function pedidosAtendidos(parent, args, context) {
-	return await context.prisma.personal
-		.findOne({ where: { id: parent.id } })
-		.pedidosAtendidos();
+async function pedidosAtendidos({ id }, args, { prisma }) {
+	return await prisma.personal.findOne({ where: { id } }).pedidosAtendidos({
+		skip: (args.pagina - 1) * args.cantidad || undefined,
+		take: args.cantidad,
+	});
 }
 
-async function listarPersonal(parent, args, context) {
-	return await context.prisma.personal.findMany();
+async function listarPersonal(parent, args, { usuario, prisma }) {
+	if (usuario.rol !== 'ADMIN') throw new AuthenticationError(NO_ADMIN);
+	return await context.prisma.personal.findMany({
+		where: { nombre: { contains: args.filtro } },
+		skip: (args.pagina - 1) * args.cantidad || undefined,
+		take: args.cantidad,
+	});
 }
 
-async function registrarPersonal(parent, args, context) {
-	const data = {
-		nombres: args.nombres,
-		apellidos: args.apellidos,
-		sueldo: parseFloat(args.sueldo),
-		cargo: { connect: { id: parseInt(args.cargo) } },
-	};
-	return await context.prisma.personal.create({ data }).catch((err) => null);
-}
-
-async function modificarPersonal(parent, args, context) {
-	const data = {};
-	if (args.nombres) data.nombres = args.nombres;
-	if (args.apellidos) data.apellidos = args.apellidos;
-	if (args.sueldo) data.sueldo = parseFloat(args.sueldo);
-	if (args.cargo) data.cargo = { connect: { id: parseInt(args.cargo) } };
-	if (args.estado != null) {
-		data.estado = args.estado;
-		if (args.estado == false) {
-			data.credencial = { update: { estado: false } };
-		}
-	}
-	return await context.prisma.personal
-		.update({
-			where: { id: parseInt(args.id) },
-			data,
+async function registrarPersonal(parent, args, { prisma }) {
+	return await prisma.personal
+		.create({
+			data: {
+				nombres: args.nombres,
+				apellidos: args.apellidos,
+				sueldo: parseFloat(args.sueldo),
+				cargo: { connect: { id: parseInt(args.cargo) } },
+			},
 		})
 		.catch((err) => null);
 }
 
-async function eliminarPersonal(parent, args, context) {
-	const numeroPedidos = await context.prisma.pedido.count({
-		where: { personalId: parseInt(args.id) },
-	});
-	if (numeroPedidos == 0) {
-		return await context.prisma.personal
-			.delete({ where: { id: parseInt(args.id) } })
-			.catch((err) => null);
-	} else {
-		return await context.prisma.personal.update({
+async function modificarPersonal(parent, args, { prisma }) {
+	return await prisma.personal
+		.update({
 			where: { id: parseInt(args.id) },
 			data: {
-				estado: false,
+				nombres: args.nombres,
+				apellidos: args.apellidos,
+				sueldo: args.sueldo,
+				cargo: { connect: { id: args.cargo } },
+				estado: args.estado,
 			},
+		})
+		.catch((err) => null);
+}
+
+async function eliminarPersonal(parent, { id }, { prisma }) {
+	const numeroPedidos = await prisma.pedido.count({
+		where: { personalId: parseInt(id) },
+	});
+	if (numeroPedidos == 0) {
+		return await prisma.personal
+			.delete({ where: { id: parseInt(id) } })
+			.catch((err) => null);
+	} else {
+		return await prisma.personal.update({
+			where: { id: parseInt(id) },
+			data: { estado: false },
 		});
 	}
 }
